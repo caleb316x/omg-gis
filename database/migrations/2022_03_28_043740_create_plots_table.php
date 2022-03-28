4b6f1db7371b903879fe4fac5713965d9d190e39
caleb316x <hyperactive789@gmail.com>
2022-03-28 13:01:08 +0800

    add plot and coordinate model, controller, migration

---
+++ database/migrations/2022_03_28_043740_create_plots_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePlotsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('plots', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(User::class)->nullable()->constrained();
+            $table->string('name');
+            $table->integer('type')->nullable();
+            $table->float('price')->nullable();
+            $table->integer('status')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('plots');
+    }
+}
---
+++ database/migrations/2022_03_28_044752_create_coordinates_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Models\Plot;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCoordinatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('coordinates', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Plot::class)->nullable()->constrained();
+            $table->double('lat');
+            $table->double('long');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('coordinates');
+    }
+}
